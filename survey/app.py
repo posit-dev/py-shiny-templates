bb05d01b9df5ea8649911d7990049b478bd31649
Gordon Shotwell <gordon.shotwell@posit.co>
2023-12-08 10:34:16 -0400

    Add value checking

--- survey/app.py
+++ survey/app.py
@@ -2,7 +2,7 @@ from shiny import Inputs, Outputs, Session, App, reactive, render, ui
 from htmltools import Tag
 import pandas as pd
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict
 
 output_path = Path(__file__).parent / "output.csv"
 
@@ -13,7 +13,7 @@ def make_likart(id: str, movie: str) -> Tag:
         f"On a scale of 1-5 how would you rate: '{movie}'?",
         1,
         5,
-        3,
+        value=None,
         ticks=True,
         step=1,
     )
@@ -32,23 +32,29 @@ ids = [movie.lower().replace(" ", "_") for movie in movies]
 
 questions = {
     "Demographics": {
-        "ids": ["name", "country"],
+        "ids": ["name", "country", "age"],
         "elements": [
-            ui.tooltip(ui.input_text("name", "Enter your name"), "Enter your name"),
+            ui.tooltip(
+                ui.input_text("name", "Enter your name"),
+                "Enter your name",
+                value=None,
+            ),
             ui.tooltip(
                 ui.input_select(
                     "country",
                     "Country",
                     choices=["USA", "Canada", "Portugal"],
+                    selectize=True,
+                    selected=None,
                 ),
                 "Your primary residence",
             ),
-            ui.input_numeric("age", "Age", value=0),
+            ui.input_numeric("age", "Age", value=None),
         ],
     },
     "Income": {
         "ids": ["income"],
-        "elements": [ui.input_slider("income", "Annual income", 0, 400000, 70000)],
+        "elements": [ui.input_numeric("income", "Annual income", value=None)],
     },
     "Ratings": {
         "ids": ids,
@@ -64,27 +70,47 @@ app_ui = ui.page_fluid(
             *[make_section(key, questions) for key in questions.keys()],
         ),
     ),
-    ui.card(
-        ui.input_action_button("save_button", "Submit"),
-    ),
+    ui.card(ui.output_ui("submit_card")),
 )
 
 
-def server(input: Inputs, output, session):
+def server(input: Inputs, output: Outputs, session: Session):
+    all_ids = [
+        id
+        for sublist in [questions[category]["ids"] for category in questions]
+        for id in sublist
+    ]
+
+    @render.ui
+    @reactive.event(*[input[id] for id in all_ids])
+    def submit_card():
+        def none_or_empty(x) -> bool:
+            return x == "" or x is None
+
+        incomplete = [none_or_empty(input[id]()) for id in all_ids]
+
+        if any(incomplete):
+            incomplete_ids = [id for id in all_ids if none_or_empty(input[id]())]
+            return ui.h4(
+                f" Mandatory fields still incomplete: {', '.join(incomplete_ids)}"
+            )
+
+        return ui.input_action_button(
+            "submit",
+            "Submit",
+            _class="btn btn-success",
+        )
+
     @reactive.Effect
-    @reactive.event(input.save_button)
+    @reactive.event(input.submit)
     def save_to_csv():
-        all_ids = [
-            id
-            for sublist in [questions[category]["ids"] for category in questions]
-            for id in sublist
-        ]
         to_save = {id: input[id]() for id in all_ids}
         df = pd.DataFrame([to_save])
         if not output_path.exists():
             df.to_csv(output_path, mode="a", header=True)
         else:
             df.to_csv(output_path, mode="a", header=False)
+        ui.modal_show(ui.modal("Form submitted!"))
 
 
 app = App(app_ui, server)
