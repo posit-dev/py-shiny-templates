10b02f20da92beb348b830be2568df62c5363bd3
Carson <cpsievert1@gmail.com>
2024-02-23 18:12:04 -0600

    Flesh out nba-dashboard concept

--- nba-dashboard/app-core.py
+++ nba-dashboard/app-core.py
@@ -1,95 +1,178 @@
 import pandas as pd
+import numpy as np
 from pathlib import Path
 
-from shiny import App, reactive, render, req, ui
+from shiny import App, reactive, req, ui
 
 from shinywidgets import output_widget, render_plotly
 import plotly.graph_objects as go
+import plotly.express as px
+import plotly.figure_factory as ff
 
-
+# Load data
 app_dir = Path(__file__).parent
-players_df = pd.read_csv(app_dir / "players.csv")
+players_df = pd.read_csv(app_dir / "players.csv", dtype={"person_id": str})
+careers = pd.read_csv(app_dir / "careers.csv", dtype={"person_id": str})
+
+# Define the stats to compare
+stats = careers.columns[1:].tolist()
 
 # create dictionary where key is player id and value is name
 players_dict = dict(zip(
-    players_df["person_id"].astype(str),
+    players_df["person_id"],
     players_df["display_first_last"]
 ))
 
-app_ui = ui.page_sidebar(
-    ui.sidebar(
-        ui.input_selectize(
-            "players",
-            "Select players",
-            multiple=True,
-            choices=players_dict,
-            selected=["893"],
-        ),
+careers["player_name"] = careers["person_id"].map(players_dict)
+
+# Define the app UI
+app_ui = ui.page_fixed(
+    ui.panel_title("NBA Player Stats"),
+    ui.input_selectize(
+        "players",
+        "Search for players",
+        multiple=True,
+        choices=players_dict,
+        selected=["893", "2544", "201939"],
+        width="100%"
     ),
     ui.card(
-        ui.card_header("Player comparison"),
-        output_widget("all_stats"),
+        ui.card_header("Player career comparison"),
+        output_widget("career_compare"),
+        ui.card_footer("Percentiles are based on career per game averages."),
         full_screen=True,
     ),
+    ui.card(
+        ui.card_header(
+            "Player career ",
+            ui.input_select(
+                "stat", None,
+                choices=stats,
+                selected="PTS",
+                width="auto"
+            ),
+            " vs the rest of the league",
+            class_="d-flex align-items-center gap-1",
+        ),
+        output_widget("stat_compare"),
+        ui.card_footer("Click on a player's name to add them to the comparison."),
+        full_screen=True,
+    ),
+    ui.include_css(app_dir / "styles.css"),
     title="NBA Dashboard",
     fillable=True,
 )
 
 
 def server(input, output, session):
-    
-    # Career stats for each player
-    # (gets read once the session starts so that this operation doesn't block the UI)
-    careers = pd.read_csv(app_dir / "careers.csv")
 
-    # One row per player (average over seasons)
-    careers = careers.groupby("person_id").mean()
-
+    # Get the stats for the selected players
     @reactive.calc
     def player_stats():
-        req(input.players())
-        return careers[careers["person_id"].isin(input.players())]
-    
-    stats = [
-        "PTS", "FG_PCT", "FG3_PCT", "FT_PCT", "REB", "AST", "STL", "BLK"
-    ]
+        players = req(input.players())
+        res = careers[careers["person_id"].isin(players)]
+        res["color"] = np.resize(px.colors.qualitative.D3, len(players))
+        return res
 
     # For each player, get the percentile of each stat
     @reactive.calc
     def percentiles():
         d = player_stats()
-        # For each player stat, get the overall percentile of that stat
-        for index, row in d.iterrows():
-            for stat in stats:
-                d.at[index, stat] = (d[stat] > row[stat]).mean()
-        return d
+
+        def apply_func(x):
+            for col in stats:
+                x[col] = (x[col].values > careers[col].values).mean()
+            return x
+
+        return d.groupby("person_id").apply(apply_func)
 
     # radar chart of player stats
     @render_plotly
-    def all_stats():
-        #import plotly.express as px
-        
-        d = percentiles()
+    def career_compare():
+        percs = percentiles()
+        stats_df = player_stats()
+
         fig = go.Figure()
 
-        for index, row in d.iterrows():
-            fig.add_trace(go.Scatter(
-                r=[row[stat] for stat in stats],
-                theta=stats,
-                fill='toself',
-                name=players_dict[index]
+        for _, row in percs.iterrows():
+            id = row["person_id"]
+            r = [row[x] for x in stats]
+            vals = stats_df[stats_df["person_id"] == id][stats].values[0]
+            text = np.round(vals, 2).astype(str).tolist()
+            fig.add_trace(go.Scatterpolar(
+                r=r + r[:1],
+                theta=stats + stats[:1],
+                text=text + text[:1],
+                name=row["player_name"],
+                hoverinfo="text+name",
+                line=dict(width=1, color=row["color"]),
             ))
 
         fig.update_layout(
-            polar=dict(
-                radialaxis=dict(
-                    visible=True,
-                    range=[0, 1]
-                )),
-            showlegend=True
+            margin=dict(l=30, r=30, t=30, b=30),
+            polar=dict(radialaxis=dict(range=[0, 1])),
+            showlegend=True,
+            legend=dict(
+                orientation="h",
+                y=-0.05, yanchor="top",
+                x=0.5, xanchor="center"
+            )
         )
 
         return fig
+    
+    # 1D density plot of player stats
+    @render_plotly
+    def stat_compare():
+        vals = careers[input.stat()]
+        vals = vals[~vals.isnull()]
+        fig = ff.create_distplot(
+            [vals], ["Overall"],
+            rug_text=[careers["player_name"]],
+            colors=["black"], show_hist=False,
+        )
+        # Clean up some defaults (1st trace is the density plot, 2nd is the rug plot)
+        fig.data[0].hoverinfo = "none"
+        fig.data[0].showlegend = False
+        fig.data[1].hoverinfo = "text+x"
+        fig.data[1].customdata = careers["person_id"]
+        # Use height of the density plot to inform the vertical lines
+        ymax = fig.data[0].y.max()
+        # Arrange rows from highest to lowest value so that legend order is correct
+        stats_df = player_stats().sort_values(input.stat(), ascending=False)
+        # Add vertical lines for each player
+        for _, row in stats_df.iterrows():
+            x = row[input.stat()]
+            fig.add_scatter(
+                x=[x, x], y=[0, ymax],
+                mode="lines", name=players_dict[row["person_id"]],
+                line=dict(color=row["color"], width=1),
+                hoverinfo="x+name"
+            )
+
+        fig.update_layout(
+            hovermode="x",
+            xaxis=dict(
+                title=input.stat() + " per game (career average)",
+                hoverformat=".1f"
+            ),
+            legend=dict(
+                orientation="h",
+                y=1.01, yanchor="bottom",
+                x=0.5, xanchor="center"
+            )
+        )
+
+        # Convert Figure to FigureWidget so we can add click events
+        fig = go.FigureWidget(fig.data, fig.layout)
+        fig.data[1].on_click(on_rug_click)
+        return fig
+
+    # When a player is clicked on the rug plot, add them to the selected players
+    def on_rug_click(trace, points, state):
+        player_id = trace.customdata[points.point_inds[0]]
+        selected = list(input.players()) + [player_id]
+        ui.update_selectize("players", selected=selected)
 
 
 app = App(app_ui, server)
--- nba-dashboard/etl.py
+++ nba-dashboard/etl.py
@@ -10,11 +10,8 @@ players["person_id"] = players["person_id"].astype(str)
 f = Path(__file__).parent / "players.csv"
 players.to_csv(f, index=False)
 
-# Get career stats for each player, and store in one big dataframe
-stat_columns = [
-    "person_id", "PTS", "FG_PCT", "FG3_PCT", "FT_PCT", "REB", "AST", "STL", "BLK", #"TOV", "PF", "PLUS_MINUS"
-]
-careers = pd.DataFrame(columns=stat_columns)
+# Get career stats for each player (one row per player per season)
+careers = pd.DataFrame()
 for id in players["person_id"]:
     print("Getting stats for player", id)
     stats = playercareerstats.PlayerCareerStats(player_id=str(id))
@@ -23,6 +20,27 @@ for id in players["person_id"]:
     careers = pd.concat([careers, stats], axis=0)
     # Avoid getting rate-limited
     time.sleep(1)
+
+f = Path(__file__).parent / "careers_all.csv"
+careers.to_csv(f, index=False)
+
+
+# Columns to use for the visualizations
+stat_cols = ["PTS", "FG_PCT", "FG3_PCT", "FT_PCT", "REB", "AST", "STL", "BLK"]
+cols = ["person_id", "GP"] + stat_cols
+
+# Divide each non-pct stat by the number of games played (to get per-game averages)
+careers["PTS"] = careers["PTS"] / careers["GP"]
+careers["REB"] = careers["REB"] / careers["GP"]
+careers["AST"] = careers["AST"] / careers["GP"]
+careers["STL"] = careers["STL"] / careers["GP"]
+careers["BLK"] = careers["BLK"] / careers["GP"]
+
+# Get the average of each stat for each player
+careers = careers[cols].groupby("person_id").mean().reset_index()
+
+# Drop the GP column
+careers = careers.drop(columns="GP")
 
 f = Path(__file__).parent / "careers.csv"
 careers.to_csv(f, index=False)
