dc4a0e3de5b008f8efbb26d1aa2fd7f24c298df1
Karan Gathani <karan.gathani@posit.co>
2025-05-27 12:48:16 -0700

    use similar method as app-express.py

--- stock-app/app-core.py
+++ stock-app/app-core.py
@@ -117,8 +117,7 @@ def server(input: Inputs, output: Outputs, session: Session):
 
     @render.data_frame
     def latest_data():
-        data = get_data().iloc[-1:]
-        x = data.T.reset_index()
+        x = get_data()[:1].T.reset_index()
         x.columns = ["Category", "Value"]
         x["Value"] = x["Value"].apply(lambda v: f"{v:.1f}")
         return x
